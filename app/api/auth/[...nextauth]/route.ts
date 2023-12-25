c9cfff0abc6289bc88243ec05313c92360e911f9
NehemieMbg <nehemie.mbg@gmail.com>
2023-12-25 20:29:51 +0100

    Refactore nextAuth file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,51 +1,5 @@
-import CredentialsProvider from 'next-auth/providers/credentials';
-import NextAuth, { AuthOptions } from 'next-auth';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '@/app/utils/prisma';
-import bcrypt from 'bcryptjs';
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        email: { label: 'Email', type: 'email' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        const { email, password } = credentials as {
-          email: string;
-          password: string;
-        };
-
-        const user = await prisma.user.findUnique({
-          where: { email },
-        });
-
-        if (!email || !password)
-          throw new Error(
-            "l'adresse e-mail et le mot de passe sont requis pour procéder."
-          );
-
-        if (!user)
-          throw new Error("Aucun utilisateur correspondant n'a été trouvé.");
-
-        if (!(await bcrypt.compare(password, user.password)))
-          throw new Error(
-            " L'adresse e-mail ou le mot de passe ne correspond pas"
-          );
-
-        return user;
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: 'jwt',
-  },
-  debug: process.env.NODE_ENV === 'development',
-};
+import { authOptions } from '@/app/utils/authOptions';
+import NextAuth from 'next-auth/next';
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
