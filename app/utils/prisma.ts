75bc208a26150b1effb6a3167b45e26437e44ec5
NehemieMbg <nehemie.mbg@gmail.com>
2024-01-02 17:13:46 +0100

    prisma

--- app/utils/prisma.ts
+++ app/utils/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 
 const prisma = new PrismaClient();
 
