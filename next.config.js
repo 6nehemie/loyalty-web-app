3438272182b7aa0f523be709afb0e490ea3eb79e
NehemieMbg <nehemie.mbg@gmail.com>
2024-01-02 16:19:29 +0100

    save

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
+const withTM = require('next-transpile-modules')([
+  '@react-email/render',
+  'html-to-text',
+  'entities',
+]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['utfs.io', 'res.cloudinary.com'],
   },
-  //   async redirects() {
-  // 	return [
-  // 	  {
-  // 		source: '/blog',
-  // 		destination: '/',
-  // 		permanent: true,
-  // 	  },
-  // 	]
-  //   },
 };
 
-module.exports = nextConfig;
+module.exports = withTM(nextConfig);
